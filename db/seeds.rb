47000068b3911658a3868bf50b0aa1284afe2b77
Adam Dren <adamdren@gmail.com>
2023-01-05 19:03:00 +1100

    added destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+puts "
+  ============================
+   Clearing DB.
+  ============================
+"
+User.destroy_all
+Infection.destroy_all
+Result.destroy_all
+ResultsInfection.destroy_all
+
+puts "
+  ============================
+   DB cleared.
+  ============================
+"
+
 require "open-uri"
 require "faker"
 
