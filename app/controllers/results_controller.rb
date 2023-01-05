265768d9cafc3cfc224d0ee42f932d04c748d032
reuben frith <reubenfrith007@gmail.com>
2023-01-05 22:06:51 +1100

    created model and started rendering results

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,16 @@
+class ResultsController < ApplicationController
+
+  def show
+    result = Result.find(params[:id])
+    @result = current_user == result.user ? result : "Access Denied"
+  end
+
+  private
+
+  def results_params
+    params.require(:result).permit(
+      :user, :doctor, :test_date, :next_test_date
+    )
+  end
+
+end
