f6274228626a3dcc5ed76a7d89cca1d2d9deacad
reuben frith <reubenfrith007@gmail.com>
2023-01-10 22:13:36 +1100

    making single view

---
+++ app/controllers/results_infections_controller.rb
@@ -0,0 +1,9 @@
+class ResultsInfectionsController < ApplicationController
+
+  def show
+    @result = Result.find(params[:result_id])
+    @result_infection = ResultsInfection.find(params[:id])
+    @infection = Infection.find(@result_infection.infection_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :results, only: %i[show]
+  resources :results, only: %i[show] do
+    resources :results_infections, only: %i[show]
+  end
 
 end
