27238c9bc1aa1fb25f4f6d5409c72df3093aea1f
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2023-01-24 16:43:37 +0800

    Still working on reveal functionality for new result form

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -2,31 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 // const infections = ["gonorrhoea", "chlamydia"]
 
 export default class extends Controller {
-  // static targets = infections.flatMap( infection => [`${infection}Target`,
-  //  `reveal${infection}Results`])
-
-  static targets = ["gonorrhoeaTarget", "revealgonorrhoeaResults"]
-
+  static targets = ["gonorrhoeaTarget", "revealgonorrhoeaResults"];
   connect() {
     console.log("The Results Controller is now loaded");
-    console.log(this.targets);
-    console.log(this.gonorrhoeaTarget);
-    console.log(this.revealgonorrhoeaResults);
+    console.log(this.gonorrhoeaTargetTarget);
+    console.log(this.revealgonorrhoeaResultsTarget);
+    this.revealgonorrhoeaResults()
   }
 
-  // revealGonorrhoeaResults() {
-  //   if (this.gonorrhoeaTarget.checked) {
-  //     this.revealgonorrhoeaResults.style.display = "block";
+  //  revealgonorrhoeaResults() {
+  //   if (this.gonorrhoeaTargetTarget.checked) {
+  //     this.revealgonorrhoeaResultsTarget.style.display = "block";
   //   } else {
-  //     this.revealgonorrhoeaResults.style.display = "none";
+  //     this.revealgonorrhoeaResultsTarget.style.display = "none";
   //   }
   // }
 
-  // revealChlamydiaResults() {
-  //   if (this.chlamydiaTarget.checked) {
-  //     this.revealchlamydiaResults.style.display = "block";
-  //   } else {
-  //     this.revealchlamydiaResults.style.display = "none";
-  //   }
-  // }
+  revealgonorrhoeaResults() {
+    const elements = querySelectorAll("revealgonorrhoeaResults");
+      if (this.gonorrhoeaTargetTarget.checked) {
+        elements.forEach(element => element.style.display = "block")
+      } else {
+        elements.forEach(element => element.style.display = "none")
+      }
+    }
 }
