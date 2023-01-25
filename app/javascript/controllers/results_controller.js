02cc4e3176e32e2bb72d672accc41b65a7ba8c40
reuben frith <reubenfrith007@gmail.com>
2023-01-25 23:00:37 +1100

    merged adding changes like route, method, return json, and x-csrf in meta

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -1,23 +1,26 @@
-// document.getElementById("results-form").addEventListener("submit", function(event) {
-//   event.preventDefault(); // prevent form submission
-//   const secureCode = document.getElementById("secure_code").value;
-//   const dateOfBirth = document.getElementById("date_of_birth").value;
-//   // Send an HTTP request to your server to validate the secure code and date of birth
-//   fetch('/validate_secure_code', {
-//     method: 'POST',
-//     body: JSON.stringify({ secure_code: secureCode, date_of_birth: dateOfBirth }),
-//     headers: { 'Content-Type': 'application/json' },
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     if (data.valid) {
-//       // Secure code is valid, proceed with form submission
-//       document.getElementById("main_form").classList.remove("hidden");
-//       document.getElementById("secure_form").classList.add("hidden");
-//     } else {
-//       // Secure code is invalid, show an error message
-//       document.getElementById("secure-code-error").innerHTML = data.error;
-//       document.getElementById("secure-code-error").classList.remove("hidden");
-//     }
-//   })
-// });
+document.getElementById("results-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent form submission
+  const secureCode = document.getElementById("secure_code").value;
+  const dateOfBirth = document.getElementById("date_of_birth").value;
+  const csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  // Send an HTTP request to your server to validate the secure code and date of birth
+  fetch('/results/validate', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json',
+    'X-CSRF-Token': csrf_token },
+    body: JSON.stringify({ secure_code: secureCode, date_of_birth: dateOfBirth })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    if (typeof data.id === 'number' && data.id >= 0) {
+      // Secure code is valid, proceed with form submission
+      document.getElementById("main_form").classList.remove("hidden");
+      document.getElementById("secure_form").classList.add("hidden");
+    } else {
+      // Secure code is invalid, show an error message
+      document.getElementById("secure-code-error").innerHTML = data.error;
+      document.getElementById("secure-code-error").classList.remove("hidden");
+    }
+  })
+});
