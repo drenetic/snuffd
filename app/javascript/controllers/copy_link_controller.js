58d5e5401cf6e0abf3bf642b8543e39c13da22ca
Reuben Frith <reubenfrith007@gmail.com>
2023-01-26 21:26:43 +1100

    Link routing (#19)
    * working copy link
    
    * cleaning
    
    * testing

---
+++ app/javascript/controllers/copy_link_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "source" ]
+  copy() {
+    navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+}
