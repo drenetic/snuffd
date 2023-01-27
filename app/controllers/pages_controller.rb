39b9f7d2d9abf013854c9de7bc07ae615a314ebb
reuben frith <reubenfrith007@gmail.com>
2023-01-28 10:48:50 +1100

    added in last test date for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      # @patient = User.find(params[:patient_id])
+      user = User.find(current_user.id)
+      @last_result = Result.where(user_id: user.id).last
+
+    end
   end
+
 end
