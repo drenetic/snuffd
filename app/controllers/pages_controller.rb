3c57c2da824214856cf26f0e15ef88068b6e2d9d
Reuben Frith <reubenfrith007@gmail.com>
2023-01-28 15:03:56 +1100

    finished (#47)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,11 @@ class PagesController < ApplicationController
     if user_signed_in?
       # @patient = User.find(params[:patient_id])
       user = User.find(current_user.id)
-      @last_result = Result.where(user_id: user.id).last
-
+      if Result.where(user_id: user.id).exists?
+        @last_result = Result.where(user_id: user.id).last.next_test_date
+      else
+        @last_result = nil
+      end
     end
   end
 
